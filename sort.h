333521d5286706b7cdcb79096a20e737efdc8d36
Ephyz8 <ziwoyaephraim@gmail.com>
2024-01-23 02:23:11 +0200

    Swapping nodes

--- sort.h
+++ sort.h
@@ -38,5 +38,6 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void swap_integers(int *n, int *m);
+void insertion_sort_list(listint_t **list);
 
 #endif
